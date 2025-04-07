6eb244325f6e4961ce7ea1d2b2d10c4bfb7cfb4a
thevowels <aungphyohtet1@gmail.com>
2025-04-07 09:14:11 +0700

    MTM-8 Create Debt Factory

--- database/factories/DebtFactory.php
+++ database/factories/DebtFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Consumer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,10 @@ class DebtFactory extends Factory
     public function definition(): array
     {
         return [
-
+            'type' => fake()->randomElement(['kyat', 'baht']),
+            'consumer_id' => Consumer::factory(),
+            'amount' => 0,
+            'limit' => 10000
         ];
     }
 }
