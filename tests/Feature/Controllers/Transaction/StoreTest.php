d8610fe9201d521a33deddb61856f4be1be45c41
thevowels <aungphyohtet1@gmail.com>
2025-04-11 12:28:31 +0700

    Fix: Transaction/Storetest. Need to create Userlimit record for testing

--- tests/Feature/Controllers/Transaction/StoreTest.php
+++ tests/Feature/Controllers/Transaction/StoreTest.php
@@ -4,6 +4,7 @@ use App\Models\Consumer;
 use App\Models\Debt;
 use App\Models\User;
 
+use App\Models\UserLimit;
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\post;
 
@@ -20,39 +21,40 @@ it('requires authentication', function () {
     ])->assertRedirect(route('login'));
 });
 
-// it('can store a transaction', function () {
-//
-//    $user = User::factory()->create();
-//
-//    $consumer = Consumer::factory()->for($user)->create();
-//
-//    $debt= Debt::factory()->for($consumer)->create();
-//    actingAs($user)
-//        ->post(route('debts.transactions.store', $debt->id), [
-//            'amount' => 3000,
-//            'type' => 'loan'
-//        ]);
-//    $this->assertDatabaseHas('transactions', [
-//        'amount' => 3000,
-//        'type' => 'loan',
-//        'debt_id' => $debt->id,
-//        'user_id' => $user->id,
-//    ]);
-// });
-//
-// it('redirects to correct debt show page', function () {
-//    $user = User::factory()->create();
-//
-//    $consumer = Consumer::factory()->for($user)->create();
-//
-//    $debt= Debt::factory()->for($consumer)->create();
-//    actingAs($user)
-//        ->post(route('debts.transactions.store', $debt->id), [
-//            'amount' => 3000,
-//            'type' => 'loan'
-//        ])
-//        ->assertRedirect(route('debts.show', $debt->id));
-// });
+ it('can store a transaction', function () {
+
+    $user = User::factory()->create();
+     UserLimit::create(['user_id' => $user->id, 'currency' => 'kyat', 'limit' => 300000]);
+     $consumer = Consumer::factory()->for($user)->create();
+
+    $debt= Debt::factory()->for($consumer)->create(['currency'=> 'kyat']);
+    actingAs($user)
+        ->post(route('debts.transactions.store', $debt->id), [
+            'amount' => 3000,
+            'type' => 'loan'
+        ]);
+    $this->assertDatabaseHas('transactions', [
+        'amount' => 3000,
+        'type' => 'loan',
+        'debt_id' => $debt->id,
+        'user_id' => $user->id,
+    ]);
+ });
+
+ it('redirects to correct debt show page', function () {
+    $user = User::factory()->create();
+    UserLimit::create(['user_id' => $user->id, 'currency' => 'kyat', 'limit' => 300000]);
+
+    $consumer = Consumer::factory()->for($user)->create();
+
+    $debt= Debt::factory()->for($consumer)->create(['currency'=>'kyat']);
+    actingAs($user)
+        ->post(route('debts.transactions.store', $debt->id), [
+            'amount' => 3000,
+            'type' => 'loan'
+        ])
+        ->assertRedirect(route('debts.show', $debt->id));
+ });
 
 it('reject invalid data', function (array $badData, array|string $errors) {
     $user = User::factory()->create();
